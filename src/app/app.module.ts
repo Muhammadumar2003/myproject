88ff083b0fb67549aeee084c172e4d7340961837
Muhammadumar2003 <mrpascal03@gmail.com>
2020-03-17 09:50:15 +0500

    Menga ajratilgan qismning yarmi bitdi!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ItemComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
